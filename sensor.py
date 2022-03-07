1ad331c15a6608a5b13dd8f9b9fa77b6c5a6ddaa
svhelge <79386304+svhelge@users.noreply.github.com>
2022-03-07 19:55:02 +0100

    Replace device_state_attributes with extra_state_attributes -
    Avoid warnings in Home Assistant log:
    Entity sensor.skisporet_xxxx (<class 'custom_components.skisporet.sensor.SkisporetSensor'>) implements device_state_attributes. Please report it to the custom component author. 
    See  https://github.com/home-assistant/core/pull/55734 and https://github.com/home-assistant/core/pull/47304

--- sensor.py
+++ sensor.py
@@ -92,7 +92,7 @@ class SkisporetSensor(Entity):
         return self._state
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes."""
         return {
                 ATTR_DISTANCE: self._distance,
