12f22946119b975ea5eceddb3c0f635783c2ce07
Ola Thoresen <ola@nytt.no>
2023-02-17 11:36:55 +0100

    Working version

--- custom_components/skisporet/__init__.py
+++ custom_components/skisporet/__init__.py
@@ -1,1 +1,25 @@
 """The skisporet component."""
+
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.config_entries import ConfigEntry
+
+from .const import DOMAIN
+
+PLATFORMS = ["sensor"]
+
+
+def setup(hass: HomeAssistant, config: ConfigEntry):
+    """Set up this using config flow."""
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up skiporet from config entry"""
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
