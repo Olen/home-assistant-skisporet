dd1813ad0a500aff416f90326337e70ff69b72fa
Ola Thoresen <ola@nytt.no>
2023-02-17 13:15:50 +0100

    Add translation and strings

---
+++ custom_components/skisporet/const.py
@@ -0,0 +1,9 @@
+"""Constants for skisporet"""
+
+ATTR_DISTANCE = "distance"
+ATTR_SEGMENT_ID = "segment_id"
+CONF_TRACK_ID = "track_id"
+ATTR_TRAIL_NAME = "trail_name"
+ATTR_TRAIL_TYPE = "trail_type"
+DOMAIN = "skisporet"
+ICON = "mdi:ski"
